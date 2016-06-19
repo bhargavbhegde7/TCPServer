fe6397f0538b5c57336f14a2e9dcc81efd132bee
bhargav <bhargavbhegde7@gmail.com>
2016-06-20 00:35:16 +0530

    first commit

---
+++ Server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python           # This is server.py file
+
+import socket
+from thread import *
+import logging
+import sys
+
+def runCommand(command,conn):
+    print "executing ", command
+    print "done running . . . "
+    print "ending . . . "
+    conn.send("ran the command")
+    return
+
+def clientThread(conn,addr):
+    print "Got connection from : " + str(addr)
+    conn.send("ack : connected")
+    while True:
+        try:
+            message = str(conn.recv(1024)).strip() #1024 bytes
+            print message," by ",addr
+            if not message or message.lower()=='quit':
+                print "Terminating connection . . . "
+                break   #connection closes as soon as break from the loop
+            else:   #incoming normal command message
+                try:
+                    start_new_thread(runCommand,(message,conn)) #run the task inside a new thread
+                except:
+                    print "Exception in running command thread . . .", "Terminating connection . . . "
+                    break
+        except:
+            print "Exception in client thread . . .", "Terminating connection . . . "
+            break
+
+    conn.close()
+    return
+
+def main(argv):
+    port = int(argv[0])             #port from command line argument
+    host = ''                       #'localhost' or '127.0.0.1' or '' are all same
+    sock = socket.socket()
+    sock.bind((host, port))
+    sock.listen(1)                  #only one client
+    conn = None                     #to access conn outside the loop
+    while True:
+        print "Waiting for connection . . . \n"
+        conn, addr = sock.accept()  #waits untill a client connects
+        start_new_thread(clientThread,(conn,addr))  #start a thread for every new client
+
+    conn.close()
+    sock.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ TestClient.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+
+while True:
+    message = raw_input("enter message : ")
+    # if message == "quit":
+    #     s.send(message)
+    #     break
+    s.send(message)
+    print s.recv(1024)
+
+s.close                     # Close the socket when done
